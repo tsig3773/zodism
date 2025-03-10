2b2e01f556f1637292759ae7066f38305de2c662
n9 <tsig3773@gmail.com>
2025-03-09 19:03:30 -0700

    final

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,21 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import { theme } from './styles/theme';
-import './index.css';
+import { GlobalStyles } from './styles/GlobalStyles';
+import { ContactProvider } from './contexts/ContactContext';
+import { ConversationProvider } from './contexts/ConversationContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <GlobalStyles />
+      <ContactProvider>
+        <ConversationProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ConversationProvider>
+      </ContactProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
